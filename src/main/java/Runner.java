43563b2c04dc4705afdb487dbd40dfdbd889d7d3
tadams <tim-adams@gmx.net>
2019-11-04 01:37:44 +0100

    Modify Runner

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,4 +1,9 @@
+import de.uni_bonn.cs.tnn.importer.Pattern;
+import de.uni_bonn.cs.tnn.importer.PatternLoader;
+
+import java.io.File;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class Runner {
@@ -6,34 +11,8 @@ public class Runner {
         int[] shape = {2, 3, 3, 2};
         TransferFuncType[] functions = {TransferFuncType.IDENTITY, TransferFuncType.TANH, TransferFuncType.TANH, TransferFuncType.TANH};
         Network testMLP = new Network(shape, functions);
-
-//        double[] x = new double[2];
-//        Random r = new Random();
-//        for(int i = 0; i < x.length; i++) x[i]=r.nextDouble();
-        double[] x = {1.0, 1.0};
-        double[] teacherY = {0.5, 0.5};
-        System.out.println("X Input: "+Arrays.toString(x));
-        System.out.println("Initial Output: "+Arrays.toString(testMLP.calculateOutputs(x)));
-        System.out.println("backprop... Teacher="+Arrays.toString(teacherY));
-        testMLP.backprop(x, teacherY);
-        testMLP.backprop(x, teacherY);
-        testMLP.backprop(x, teacherY);
-        testMLP.backprop(x, teacherY);
-        testMLP.backprop(x, teacherY);
-        testMLP.backprop(x, teacherY);
-        testMLP.backprop(x, teacherY);
-        testMLP.backprop(x, teacherY);
-        testMLP.backprop(x, teacherY);
-        testMLP.backprop(x, teacherY);
-        testMLP.backprop(x, teacherY);
-        testMLP.backprop(x, teacherY);
-        testMLP.backprop(x, teacherY);
-        testMLP.backprop(x, teacherY);
-        testMLP.backprop(x, teacherY);
-        testMLP.backprop(x, teacherY);
-        testMLP.backprop(x, teacherY);
-        testMLP.backprop(x, teacherY);
-        testMLP.backprop(x, teacherY);
-        System.out.println("Trained Out: "+Arrays.toString(testMLP.calculateOutputs(x)));
+        PatternLoader loader = new PatternLoader();
+        List<Pattern> patterns = loader.loadPatterns(new File("src/main/resources/training2.dat"));
+        testMLP.train(patterns);
     }
 }
