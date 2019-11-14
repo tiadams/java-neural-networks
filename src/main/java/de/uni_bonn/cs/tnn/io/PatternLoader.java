eed1151ec52d1dde5fbb7c28b3d0f8a511274d9a
tadams <tim-adams@gmx.net>
2019-11-14 15:43:29 +0100

    Closes #1 Adapt PatternLoader and fix one error in training file

--- src/main/java/de/uni_bonn/cs/tnn/io/PatternLoader.java
+++ src/main/java/de/uni_bonn/cs/tnn/io/PatternLoader.java
@@ -5,37 +5,56 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class PatternLoader {
 
-    // TODO: adapt to multiple output values, read from header
-    public ArrayList<Pattern> loadPatterns(File file){
+    public ArrayList<Pattern> loadPatterns(File file) {
         System.out.println("Loading Patterns from file " + file.getAbsolutePath() + "...");
         ArrayList<Pattern> patterns = new ArrayList<Pattern>();
         BufferedReader br = null;
+        String headerRegex = "#.*P=\\d.*N=\\d.*M=\\d";
         try {
             br = new BufferedReader(new FileReader(file));
             String line;
-            while ((line = br.readLine()) != null){
-                if(!line.startsWith("#")){
-                    Pattern pattern = new Pattern();
-                    String[] input_output;
-                    if (line.contains("\t")){
-                        input_output = line.split("\t");
-                    } else{
-                        input_output = line.split("  ");
+            // header params
+            int p = 0;
+            int n = 0;
+            int m = 0;
+            boolean initialized = false;
+            while ((line = br.readLine()) != null) {
+                if (line.matches(headerRegex)) {
+                    String temp = line.replaceFirst("#", "");
+                    p = Integer.parseInt(temp.replaceAll("\\s*\\b(?!P=\\d)\\w+=\\d+", "").trim().split("=")[1]);
+                    n = Integer.parseInt(temp.replaceAll("\\s*\\b(?!N=\\d)\\w+=\\d+", "").trim().split("=")[1]);
+                    m = Integer.parseInt(temp.replaceAll("\\s*\\b(?!M=\\d)\\w+=\\d+", "").trim().split("=")[1]);
+                    initialized = true;
+                }
+                if (!line.startsWith("#")) {
+                    if (!initialized) {
+                        System.out.println("No header found - returning null");
+                        return null;
                     }
-                    String[] input = input_output[0].trim().split(" ");
-                    String[] output = input_output[1].trim().split(" ");
-                    pattern.setInput(Arrays.stream(input)
-                            .mapToDouble(Double::parseDouble)
-                            .toArray());
-                    pattern.setOutput(Arrays.stream(output)
-                            .mapToDouble(Double::parseDouble)
-                            .toArray());
+                    Pattern pattern = new Pattern();
+                    String[] split = Arrays.asList(line.split(" ")).stream().filter(x -> !x.isEmpty()).toArray(String[]::new);
+                    if(split.length != (n + m)){
+                        System.out.println(line);
+                    }
+                    assert (split.length == (n + m));
+                    List<Double> input = new ArrayList<>();
+                    List<Double> output = new ArrayList<>();
+                    for (int i = 0; i < split.length; i++) {
+                        if (i < n) {
+                            input.add(Double.parseDouble(split[i]));
+                        } else {
+                            output.add(Double.parseDouble(split[i]));
+                        }
+                    }
+                    pattern.setInput(input.stream().mapToDouble(Double::doubleValue).toArray());
+                    pattern.setOutput(output.stream().mapToDouble(Double::doubleValue).toArray());
                     patterns.add(pattern);
                 }
-        }
+            }
             System.out.println("Successfully parsed " + patterns.size() + " patterns.");
         } catch (IOException e) {
             e.printStackTrace();
@@ -44,10 +63,10 @@ public class PatternLoader {
     }
 
     // TODO: This does belong in its own class, possibly extending List<Pattern>
-    public List<Pattern> getShuffledPatternList(List<Pattern> patterns){
+    public List<Pattern> getShuffledPatternList(List<Pattern> patterns) {
         ArrayList<Pattern> shuffled = new ArrayList<Pattern>();
         Random rnd = new Random();
-        while(patterns.size() != 0){
+        while (patterns.size() != 0) {
             int pick = rnd.nextInt(patterns.size());
             shuffled.add(patterns.get(pick));
             patterns.remove(pick);
